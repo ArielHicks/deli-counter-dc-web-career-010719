ec2e5647ce60f1c17155d3b8d91147c2a980692b
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-21 21:38:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,21 +8,21 @@
         current_line = "The line is currently:"
         deli.each.with_index(1) do |person, i|
           current_line << " #{i}. #{person}"
+      end
+      puts current_line
     end
-    puts current_line
-  end
-end
-
-  def take_a_number(deli, name)
-    deli << name
-    puts "Welcome, #{name}. You are number #{deli.length} in line."
   end
 
-  def now_serving(deli)
-    if deli.empty?
-      puts "There is nobody waiting to be served!"
-    else
-      puts "Currently serving #{deli.first}."
-      deli.shift
+    def take_a_number(deli, name)
+      deli << name
+      puts "Welcome, #{name}. You are number #{deli.length} in line."
+    end
+
+    def now_serving(deli)
+      if deli.empty?
+        puts "There is nobody waiting to be served!"
+      else
+        puts "Currently serving #{deli.first}."
+        deli.shift
+    end
   end
-end
