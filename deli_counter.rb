9d130ce4a0b16eadb7ac81a6bada2261379a0834
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-21 21:02:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,1 @@
 # Write your code here.
-
-# Build the line method that shows everyone their current place in the line. 
-# If there is nobody in line, it should say "The line is currently empty.".
-katz_deli = []
-
-  def line
-    
-    
-  end
